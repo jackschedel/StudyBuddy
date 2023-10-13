085a8f8970a8b5f5c9b6c4c11e6d30f386938e6d
Jack Schedel <jackschedel@gmail.com>
2023-10-13 18:02:53 -0400

    yarn, tailwind init

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: [],
+  darkMode: false,
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
+
