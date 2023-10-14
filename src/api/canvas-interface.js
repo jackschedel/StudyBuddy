73d201cc1bb672146c103c9d8ed285c34e562ea1
Jack Schedel <jackschedel@gmail.com>
2023-10-14 18:46:21 -0400

    flask update for sean

--- src/api/canvas-interface.js
+++ src/api/canvas-interface.js
@@ -2,213 +2,234 @@
 const UflUrl = 'http://127.0.0.1:5000/uflproxy/';
 
 // Your Canvas API Token for authentication
-const UFL_API_KEY = "1016~V6eewhmN8zOxHP7PO1gBJaWPuLw0YfTmjrrZmhblmHQC5xBLC5q5QER4cy3w0g2a" 
+const UFL_API_KEY =
+  "1016~Ib1rAaKjrq61HmgjfpAaRRKOReFfJksW0Q7tAm79hZVIdmmjHkiWd6X2JbtHz6Mg";
 // localStorage.getItem("canvas_api_key") || null;
 
 // Function to fetch courses
 async function fetchCourses() {
-    const response = await fetch(`${UflUrl}api/v1/courses`, {
-      method: 'GET',
-      headers: {
-        'Authorization': `Bearer ${UFL_API_KEY}`
-      }
-    });
+  const response = await fetch(`${UflUrl}api/v1/courses`, {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${UFL_API_KEY}`,
+    },
+  });
 
-    return await response.json();
-  }
-  
-  // Function to fetch assignments for a given course
-  /**
- * @param {any} courseId
- */
-  async function fetchAssignments(courseId) { 
-    const validAssignments = [];
-    try {
-    const response = await fetch(`${UflUrl}api/v1/courses/${courseId}/assignments`, {
-      method: 'GET',
-      headers: {
-        'Authorization': `Bearer ${UFL_API_KEY}`
-      }
-    });
-    const assignments = await response.json();
-      for (const assignment of assignments){
-        // filter out future, old, and locked assignments
-        if (assignment.submission_types.includes('online_quiz') && assignment.locked_for_user == false 
-        && (new Date(assignment.due_at)).getTime() > (new Date("8/15/2023")).getTime() && (new Date(assignment.due_at)).getTime() < (new Date()).getTime()
-        ){
-          assignment.html_url = `${UflUrl}courses/${courseId}/assignments/${assignment.id}/history?headless=1`;
-            validAssignments.push(assignment)
-        }
-      }
-    } catch (error){
-    }
-    return validAssignments;
-  }
-  
-  /**
- * @param {any} courseId
- */
-  async function fetchQuizzes(courseId) { // for one course
-    const validQuizzes = [];
-    try {
-    const response = await fetch(`${UflUrl}api/v1/courses/${courseId}/quizzes`, {
-      method: 'GET',
-      headers: {
-        'Authorization': `Bearer ${UFL_API_KEY}`
-      }
-    });
-    const quizzes = await response.json();
-    
-      for (const quiz of quizzes){
-        // filter out future, old, hidden, and locked quizzes
-        if (quiz.hide_results != "always" && quiz.locked_for_user == false 
-        && (new Date(quiz.due_at)).getTime() > (new Date("8/15/2023")).getTime() && (new Date(quiz.due_at)).getTime() < (new Date()).getTime()
-        ){
-            quiz.html_url = `${UflUrl}courses/${courseId}/quizzes/${quiz.id}/history?headless=1`;
-            validQuizzes.push(quiz)
-        }
-      }
-    } catch (error){
-    }
-    return validQuizzes;
-  }
-
-  async function fetchAllAssignments() {  // calls fetchAssignments for each course
-    const courses = await fetchCourses();
-    const assignments = [];
-    try {
-        for (const course of courses) {
-        const newAssignments = await fetchAssignments(course.id);
-        for (const assignment of newAssignments) {
-            assignments.push(assignment);
-        }
-        }
-    } catch (error) {
-    }
-  return assignments;
-  }
-
-  async function fetchAllQuizzes() {  // calls fetchQuizzes for each course
-    const courses = await fetchCourses();
-    const quizzes = [];
-    try {
-        for (const course of courses) {
-        const newQuizzes = await fetchQuizzes(course.id);
-        
-            for (const quiz of newQuizzes) {
-                quizzes.push(quiz);
-            }
-        } 
-    } catch(error) {
-      }
-  return quizzes;
-  }
-
-async function fetchAll(){  // fetch all assignments and quizzes and sort by date them into one list
-    const quizzes = await fetchAllQuizzes();
-    const assignments = await fetchAllAssignments();
-    const newList = [];
-    try {
-        for (const quiz of quizzes) {
-            newList.push(quiz);
-        }
-    } catch (error) {
-          console.log('no quizzes');
-      }
-    try {
-        for (const assignment of assignments) {
-            newList.push(assignment);
-
-        }
-    } catch (error) {
-          console.log('no assignments');
-    }
-    
-    // sort by due date here
-    newList.sort(function(a,b){
-      return new Date(a.due_at).getTime() - new Date(b.due_at).getTime();
-  });  
-    return newList;
+  return await response.json();
 }
 
+// Function to fetch assignments for a given course
+/**
+ * @param {any} courseId
+ */
+async function fetchAssignments(courseId) {
+  const validAssignments = [];
+  try {
+    const response = await fetch(
+      `${UflUrl}api/v1/courses/${courseId}/assignments`,
+      {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${UFL_API_KEY}`,
+        },
+      },
+    );
+    const assignments = await response.json();
+
+    console.log(assignments.length);
+
+    for (const assignment of assignments){
+      if (assignment.submission_types.includes('online_quiz')
+      ){
+        assignment.html_url = `${UflUrl}courses/${courseId}/assignments/${assignment.id}/history?headless=1`;
+          validAssignments.push(assignment)
+      }
+    }
+
+  } catch (error) {}
+  console.log(validAssignments.length);
+
+  return validAssignments;
+}
 
 /**
  * @param {any} courseId
  */
-async function fetchFiles(courseId){
-    const files = [];
-    try {
-    const filesResponse = await fetch(`${UflUrl}api/v1/courses/${courseId}/files`, {
-      method: 'GET',
-      headers: {
-        'Authorization': `Bearer ${UFL_API_KEY}`
+async function fetchQuizzes(courseId) {
+  // for one course
+  const validQuizzes = [];
+  try {
+    const response = await fetch(
+      `${UflUrl}api/v1/courses/${courseId}/quizzes`,
+      {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${UFL_API_KEY}`,
+        },
+      },
+    );
+    const quizzes = await response.json();
+    console.log(quizzes);
+
+    for (const quiz of quizzes) {
+      // filter out future, old, hidden, and locked quizzes
+      if (
+        quiz.hide_results != "always" &&
+        quiz.locked_for_user == false &&
+        new Date(quiz.due_at).getTime() > new Date("8/15/2023").getTime() &&
+        new Date(quiz.due_at).getTime() < new Date().getTime()
+      ) {
+        quiz.html_url = `${UflUrl}courses/${courseId}/quizzes/${quiz.id}/history?headless=1`;
+        validQuizzes.push(quiz);
       }
-    });
+    }
+  } catch (error) {
+    console.log(error);
+  }
+  return validQuizzes;
+}
+
+async function fetchAllAssignments() {
+  // calls fetchAssignments for each course
+  const courses = await fetchCourses();
+  const assignments = [];
+  try {
+    for (const course of courses) {
+      const newAssignments = await fetchAssignments(course.id);
+      for (const assignment of newAssignments) {
+        assignments.push(assignment);
+      }
+    }
+  } catch (error) {}
+  return assignments;
+}
+
+async function fetchAllQuizzes() {
+  // calls fetchQuizzes for each course
+  const courses = await fetchCourses();
+  const quizzes = [];
+  try {
+    for (const course of courses) {
+      const newQuizzes = await fetchQuizzes(course.id);
+
+      for (const quiz of newQuizzes) {
+        quizzes.push(quiz);
+      }
+    }
+  } catch (error) {}
+  return quizzes;
+}
+
+async function fetchAll() {
+  // fetch all assignments and quizzes and sort by date them into one list
+  const quizzes = await fetchAllQuizzes();
+  const assignments = await fetchAllAssignments();
+  const newList = [];
+  try {
+    for (const quiz of quizzes) {
+      newList.push(quiz);
+    }
+  } catch (error) {
+    console.log("no quizzes");
+  }
+  try {
+    for (const assignment of assignments) {
+      newList.push(assignment);
+    }
+  } catch (error) {
+    console.log("no assignments");
+  }
+
+  // sort by due date here
+  newList.sort(function (a, b) {
+    return new Date(a.due_at) - new Date(b.due_at);
+  });
+  return newList;
+}
+
+/**
+ * @param {any} courseId
+ */
+async function fetchFiles(courseId) {
+  const files = [];
+  try {
+    const filesResponse = await fetch(
+      `${UflUrl}api/v1/courses/${courseId}/files`,
+      {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${UFL_API_KEY}`,
+        },
+      },
+    );
     const filesJSON = await filesResponse.json();
 
     //console.log(filesJSON)
-      for (const file of filesJSON){
-        // filter out future, old, hidden, and locked quizzes
-        console.log(file.folders_url + " " + file.files_url)
+    for (const file of filesJSON) {
+      // filter out future, old, hidden, and locked quizzes
+      console.log(file.folders_url + " " + file.files_url);
 
-        files.push(file)
-      }
-      const foldersResponse = await fetch(`${UflUrl}api/v1/courses/${courseId}/folders`, {
-        method: 'GET',
-        headers: {
-          'Authorization': `Bearer ${UFL_API_KEY}`
-        }
-      });
-    } catch (error){
+      files.push(file);
     }
-    return files;
+    const foldersResponse = await fetch(
+      `${UflUrl}api/v1/courses/${courseId}/folders`,
+      {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${UFL_API_KEY}`,
+        },
+      },
+    );
+  } catch (error) {}
+  return files;
 }
 
 /**
  * @param {any} folderId
  */
-async function fetchFilesByFolder(folderId){
-    const files = [];
-    try {
+async function fetchFilesByFolder(folderId) {
+  const files = [];
+  try {
     const response = await fetch(`${UflUrl}api/v1/folders/${folderId}/files`, {
-      method: 'GET',
+      method: "GET",
       headers: {
-        'Authorization': `Bearer ${UFL_API_KEY}`
-      }
+        Authorization: `Bearer ${UFL_API_KEY}`,
+      },
     });
     return await response.json();
-    } catch (error){
-    }
-    // return empty array if no files
-    return [];
+  } catch (error) {}
+  // return empty array if no files
+  return [];
 }
-async function fetchAllFiles() {  // calls fetchFiles for each course
-    const courses = await fetchCourses();
-    const files = [];
-    try {
-        for (const course of courses) {
-            const response = await fetch(`${UflUrl}api/v1/courses/${course.id}/folders`, {
-                method: 'GET',
-                headers: {
-                  'Authorization': `Bearer ${UFL_API_KEY}`
-                }
-              });
-              const newFolders = await response.json();
-              for (const folder of newFolders){
-                    const newFiles = await fetchFilesByFolder(folder.id);
-                    for (const file of newFiles){
-                        const ext = file.filename.substring(file.filename.length - 4);
-                        if (ext == ".pdf" || ext == "pptx")
-                            files.push(file);
-                    }
-              }
+async function fetchAllFiles() {
+  // calls fetchFiles for each course
+  const courses = await fetchCourses();
+  const files = [];
+  try {
+    for (const course of courses) {
+      const response = await fetch(
+        `${UflUrl}api/v1/courses/${course.id}/folders`,
+        {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${UFL_API_KEY}`,
+          },
+        },
+      );
+      const newFolders = await response.json();
+      for (const folder of newFolders) {
+        const newFiles = await fetchFilesByFolder(folder.id);
+        for (const file of newFiles) {
+          const ext = file.filename.substring(file.filename.length - 4);
+          if (ext == ".pdf" || ext == "pptx") files.push(file);
         }
-    }catch (error) {
       }
+    }
+  } catch (error) {}
   return files;
 }
-fetchCourses().then((data) => {
-    console.log(data);
-})
+fetchAssignments("490443").then((data) => {
+  console.log(data);
+});
 
 //export { fetchAll, fetchAllFiles};
