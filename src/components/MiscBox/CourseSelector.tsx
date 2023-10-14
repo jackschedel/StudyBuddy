71eafcd68d91017cdcca77e336e24fa73abf38fe
Jack Schedel <jackschedel@gmail.com>
2023-10-14 15:22:35 -0400

    tsc cleanup

--- src/components/MiscBox/CourseSelector.tsx
+++ src/components/MiscBox/CourseSelector.tsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 const CourseSelector = () => {
   return <div></div>;
 };
--- src/components/MiscBox/SettingsButton.tsx
+++ src/components/MiscBox/SettingsButton.tsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 const SettingsButton = () => {
   return <div></div>;
 };
